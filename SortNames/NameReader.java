c8878a8d5a37bbd224fc2af2caf55934463d4858
Dontae Moore <73968444+DontaeMoore@users.noreply.github.com>
2021-10-18 14:52:46 -0400

    Add files via upload

---
+++ SortNames/NameReader.java
@@ -0,0 +1,69 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class NameReader {
+
+	public static void main(String[] args) throws IOException {
+		
+		//find file
+		File file = new File("names.txt");
+
+		Scanner scan1 = new Scanner(file);
+		Scanner scan2 = new Scanner(file);
+		
+		//skip the first line because its empty
+		scan1.nextLine();
+		scan2.nextLine();
+		
+		//first scan simply tells us how big our array needs to be
+		int nameCount = 0;
+		while(scan1.hasNextLine()) {
+			nameCount++;
+			scan1.nextLine();
+			
+		}
+		
+		System.out.println("name count = " + nameCount);
+		String[] nameArray = new String[nameCount];
+		
+		//second scan will grab all names and store then in String array
+		int i = 0;
+		while(scan2.hasNextLine()) {
+			String name = scan2.nextLine();
+			nameArray[i] = name;
+			System.out.println(nameArray[i]);
+			i++;
+			
+		}
+		System.out.println();
+		System.out.println("sorted now");
+		
+		//get rid of all whitespace in words
+		for(int j = 0; j < 50; j++) {
+			nameArray[j] = nameArray[j].replaceAll("\\s", "");
+		}
+		
+		// array of values stored in nameArray, all thats left is to sort them :)
+		Arrays.sort(nameArray, Comparator.comparingInt(String::length).thenComparing(String::compareTo));
+		
+		//print when complete
+		String fileContent = "";
+		for(int j = 0; j < 50; j++) {
+			System.out.println(nameArray[j]);
+			fileContent = fileContent.concat(nameArray[j] + "\n");
+		}
+		System.out.println("File exported to sortNames");
+		
+		FileWriter writer = new FileWriter("sortedNames.txt");
+		writer.write(fileContent);
+		writer.close();
+		
+		
+	}
+
+}
---
+++ SortNames/README.txt
@@ -0,0 +1,17 @@
+Dontae Moore
+10/5/2021
+Q1 Assignment 
+
+Instructions:
+Included in my submission is a java file, the names file, and this README
+
+
+All we have to do is compile it and run it. Open the command prompt, and navigate to this folder. Ensure the jdk is installed in your windows.
+Run javac NameReader.java to compile, once done the .class file should be created inside the SortNames folder
+Run java NameReader to run the program. Outputed should be the names sorted correctly in the stout. My program also creates a file and outputs the names
+in the same folder.
+
+Program also runs in Ubuntu.
+
+
+

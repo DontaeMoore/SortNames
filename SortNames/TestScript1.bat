bbeb137c75061fc893ea8e831409b5e29e71048f
Dontae Moore <73968444+DontaeMoore@users.noreply.github.com>
2021-10-30 01:16:33 -0400

    Update TestScript1.bat

--- SortNames/TestScript1.bat
+++ SortNames/TestScript1.bat
@@ -3,5 +3,6 @@
 cd SortNames
 javac NameReader.java
 java NameReader
+echo lol
 fc /b sortedNames.txt SortedText.txt > nul
 if errorlevel 1 (echo "The value of variable c is 15") else (echo "Unknown value") 

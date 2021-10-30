0d016524bd270ae48ee84d5485019a04d540b20b
Dontae Moore <73968444+DontaeMoore@users.noreply.github.com>
2021-10-30 01:01:16 -0400

    Update TestScript1.bat

--- SortNames/TestScript1.bat
+++ SortNames/TestScript1.bat
@@ -9,4 +9,5 @@ fc /b sortedNames.txt SortedText.txt > nul
 if errorlevel 1 
     ( echo The Sorted text in sortedNames does not match the text from our already sorted text in SortedText!
 )
+echo yo
 PAUSE

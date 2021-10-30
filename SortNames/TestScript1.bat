19c947a45e85c00e99c268df82d5ac40a3fa99e1
Dontae Moore <73968444+DontaeMoore@users.noreply.github.com>
2021-10-30 00:40:08 -0400

    Update TestScript1.bat

--- SortNames/TestScript1.bat
+++ SortNames/TestScript1.bat
@@ -3,7 +3,7 @@ cd SortNames
 javac NameReader.java
 java NameReader
 fc /b sortedNames.txt SortedText.txt > nul
-if errorlevel 1 (
+if errorlevel 1(
     echo The Sorted text in sortedNames does not match the text from our already sorted text in SortedText!
 ) 
 PAUSE

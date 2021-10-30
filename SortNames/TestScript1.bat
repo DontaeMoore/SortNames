cce65a2a1c9a00d6c1233740f79e3b0dfd85e994
Dontae Moore <73968444+DontaeMoore@users.noreply.github.com>
2021-10-30 00:05:18 -0400

    Update TestScript1.bat

--- SortNames/TestScript1.bat
+++ SortNames/TestScript1.bat
@@ -4,7 +4,7 @@ javac NameReader.java
 java NameReader
 fc /b sortedNames.txt SortedText.txt > nul
 
-if errorlevel 1  (
+if errorlevel == 1  (
     echo The Sorted text in sortedNames does not match the text from our already sorted text in SortedText!
  )  else (
     echo The Sorted text in sortedNames matches the text from our already sorted text in SortedText!
@@ -13,7 +13,7 @@ if errorlevel 1  (
 java NameReader Reverse
 fc /b sortedNames.txt SortedTextReverse.txt > nul
 
-if errorlevel 1 (
+if errorlevel == 1 (
     echo The Reversed Sorted text in sortedNames does not match the text from our already reverse sorted text in SortedTextReverse!
  )  else (
     echo The Reversed Sorted text in sortedNames matches the text from our already reverse sorted text in SortedTextReverse!

8576049aa1516f4c2b74736ab1df6784517af4d7
Dontae Moore <73968444+DontaeMoore@users.noreply.github.com>
2021-10-30 01:13:32 -0400

    Update TestScript1.bat

--- SortNames/TestScript1.bat
+++ SortNames/TestScript1.bat
@@ -3,7 +3,4 @@ cd SortNames
 javac NameReader.java
 java NameReader
 fc /b sortedNames.txt SortedText.txt > nul
-SET /A a = 5 
-SET /A b = 10
-SET /A c = %a% + %b% 
-if %c%==15 (echo "The value of variable c is 15") else (echo "Unknown value") 
+if errorlevel 1 (echo "The value of variable c is 15") else (echo "Unknown value") 

2285d56033c2478d69a1a72143536e95aa163f2c
Dontae Moore <73968444+DontaeMoore@users.noreply.github.com>
2021-10-31 00:37:36 -0400

    Testing Script Commit
    All around 100 of these commits were to configure GitHub Actions to run automatic testing on this project. This should be one of the last commits before going back to a typical, correct approach involving branches

--- SortNames/TestScript.ps1
+++ SortNames/TestScript.ps1
@@ -4,8 +4,16 @@ java NameReader
 
 
 $fileA = "sortedNames.txt"
-
+$fileB = "SortedText.txt"
 $fileC = "SortedTextReverse.txt"
+
+if((Get-FileHash $fileA).hash  -ne (Get-FileHash $fileB).hash)
+
+ {"files are different"}
+
+Else {"Files are the same"}
+
+java NameReader Reverse
 
 if((Get-FileHash $fileA).hash  -ne (Get-FileHash $fileC).hash)
 

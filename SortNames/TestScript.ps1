e6fdfd722f83e28b996e6f0a93dd049863c40720
Dontae Moore <73968444+DontaeMoore@users.noreply.github.com>
2021-10-31 00:10:49 -0400

    Add files via upload

---
+++ SortNames/TestScript.ps1
@@ -0,0 +1,14 @@
+
+javac NameReader.java
+java NameReader
+
+
+$fileA = "sortedNames.txt"
+
+$fileC = "sortedTextReverse.txt"
+
+if((Get-FileHash $fileA).hash  -ne (Get-FileHash $fileC).hash)
+
+ {"files are different"}
+
+Else {"Files are the same"}
